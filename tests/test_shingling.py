545804a1d65517b9a04cd1f53a72751bdcbbaa04
javiergf14 <antoniojavier.gonzalez.ferrer@alumnos.upm.es>
2017-11-05 17:55:03 +0100

    Update minhash test with assert_almost_equal.

--- tests/test_shingling.py
+++ tests/test_shingling.py
@@ -1,4 +1,4 @@
-from nose.tools import assert_equal
+from nose.tools import assert_equal, assert_almost_equal
 
 from locality_sensitive_hashing.shingling import Shingling
 from locality_sensitive_hashing.minhashing import MinHashing
@@ -27,10 +27,10 @@ def test_minhash_similarity():
     str1_shingle = Shingling(str1, 5)
     str2_shingle = Shingling(str2, 5)
 
-    str1_minhashing = MinHashing(str1_shingle.shingles, 100)
-    str2_minhashing = MinHashing(str2_shingle.shingles, 100)
+    str1_minhashing = MinHashing(str1_shingle.shingles, 10000)
+    str2_minhashing = MinHashing(str2_shingle.shingles, 10000)
 
-    print(compare_signatures(str1_minhashing.signature, str2_minhashing.signature))
+    assert_almost_equal(compare_signatures(str1_minhashing.signature, str2_minhashing.signature), 0.25, 1)
 
 
 if __name__ == "__main__":
