9084675fa00aaa9278dc194503e62b199f6e07a3
javiergf14 <antoniojavier.gonzalez.ferrer@alumnos.upm.es>
2017-11-05 11:49:39 +0100

    Add test with nose library.

---
+++ tests/test_shingling.py
@@ -0,0 +1,18 @@
+from nose.tools import assert_equal
+
+from locality_sensitive_hashing.shingling import Shingling
+from locality_sensitive_hashing.compare import compare_sets
+
+
+def test_strings_similarity():
+    editorial = "editorial"
+    factorial = "factorial"
+    expected_values = [(1, 0.6), (5, 0.25), (9, 0.0)]
+
+    for k, expected in expected_values:
+        editorial_shingle = Shingling(editorial, k)
+        factorial_shingle = Shingling(factorial, k)
+        jaccard_similarity = compare_sets(editorial_shingle.shingles, factorial_shingle.shingles)
+
+        assert_equal(jaccard_similarity, expected, "Jaccard similarity of '{}' and '{}' failed. Expected: {}, actual: {}"
+                     .format(editorial, factorial, expected, jaccard_similarity))
